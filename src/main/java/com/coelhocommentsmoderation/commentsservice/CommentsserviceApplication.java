318bc8fd479020baabdc3c7bd4e7cf1dab172d4f
Wilson Coelho <wlfcoelho@gmail.com>
2025-05-26 08:01:54 -0300

    new project add files

---
+++ src/main/java/com/coelhocommentsmoderation/commentsservice/CommentsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.coelhocommentsmoderation.commentsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coelhocommentsmoderation/commentsservice/CommentsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coelhocommentsmoderation.commentsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
